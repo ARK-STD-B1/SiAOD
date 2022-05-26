6ecd81048be0823102dfa998e1ffc06091c6d4cc
ARK-STD-B1 <103193595+ARK-STD-B1@users.noreply.github.com>
2022-05-26 22:33:33 +0300

    Update ПР7.cpp

--- ПР7.cpp
+++ ПР7.cpp
@@ -6,36 +6,28 @@
 #include <windows.h>
 #include <iomanip>
 #include <algorithm>
-
 using namespace std;
-
 void printList(list<int> L)
 {
 	for (auto i : L)
 	{
 		cout << i << endl;
 	}
-
 }
-
 void fillList(list<int>* L, int q)
 {
-
 	for (int i = q - 1; i >= 0; i--)
 	{
 		L->push_front(rand() % 10);
 	}
 }
-
 int main()
 {
 	setlocale(0, "");
-
 	srand((unsigned)time(NULL));
 	list<int> L1, L2, L;
 	fillList(&L1, 10);
 	fillList(&L2, 10);
-
 	for (auto i : L1)
 	{
 		if (find(L2.begin(), L2.end(), i) == L2.end() && find(L.begin(), L.end(), i) == L.end())
@@ -43,7 +35,6 @@ int main()
 			L.push_front(i);
 		}
 	}
-
 	for (auto i : L2)
 	{
 		if (find(L1.begin(), L1.end(), i) == L1.end() && find(L.begin(), L.end(), i) == L.end())
@@ -51,7 +42,6 @@ int main()
 			L.push_front(i);
 		}
 	}
-
 	cout << "- L1 -"<<endl;
 	cout<<endl;
 	printList(L1);
@@ -64,6 +54,5 @@ int main()
 	cout<<endl;
 	printList(L);
 	cout<<endl;
-
 	return 0;
 }
